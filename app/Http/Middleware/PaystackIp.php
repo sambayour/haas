35543bd1390608f723afd7a44b2a71c95858b448
Samuel Olubayo <102560992+sambayo@users.noreply.github.com>
2023-03-24 14:08:15 +0100

    tests

--- app/Http/Middleware/PaystackIp.php
+++ app/Http/Middleware/PaystackIp.php
@@ -8,6 +8,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class PaystackIp
 {
+    public $whitelistIps = [
+        '52.31.139.75',
+        '52.49.173.169',
+        '52.214.14.220',
+    ];
     /**
      * Handle an incoming request.
      *
@@ -15,6 +20,9 @@ class PaystackIp
      */
     public function handle(Request $request, Closure $next): Response
     {
+        if (!in_array($request->getClientIp(), $this->whitelistIps)) {
+            abort(403, "You are restricted to access the route.");
+        }
         return $next($request);
     }
 }
